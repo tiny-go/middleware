f788c6e952afed5dffc80926f8b03c2e3e25b72c
Alexander Shcherbyna <xantax@ukr.net>
2018-02-10 18:10:55 +0100

    response headers middleware

---
+++ set_headers.go
@@ -0,0 +1,17 @@
+package mw
+
+import (
+	"net/http"
+)
+
+// SetHeaders allows to set response headers.
+func SetHeaders(headers map[string]string) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			for key, value := range headers {
+				w.Header().Set(key, value)
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
---
+++ set_headers_test.go
@@ -0,0 +1,26 @@
+package mw
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_SetHeaders(t *testing.T) {
+	t.Run("set headers middleware should set provided response headers", func(t *testing.T) {
+		headers := map[string]string{
+			"Content-Encoding": "gzip",
+			"Content-Length":   "27225",
+			"Content-Type":     "application/json",
+		}
+		handler := SetHeaders(headers)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+		w := httptest.NewRecorder()
+		handler.ServeHTTP(w, nil)
+
+		for key, value := range headers {
+			if w.Header().Get(key) != value {
+				t.Errorf("header %q expected to be %q but got %q", key, value, w.Header().Get(key))
+			}
+		}
+	})
+}
