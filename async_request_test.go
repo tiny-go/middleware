8130cd20fe1b58f7f8d00a12e7e55f45453e924a
Alexander Shcherbyna <xantax@ukr.net>
2018-02-21 09:12:23 +0100

    fix typo

--- async_request_test.go
+++ async_request_test.go
@@ -175,7 +175,7 @@ func Test_AsyncRequest(t *testing.T) {
 					data:    "request is in progress\n",
 				},
 				{
-					title:   "should store the result after task is completed and be able to return it (in cooperation with hanlder)",
+					title:   "should store the result after task is completed and be able to return it (in cooperation with handler)",
 					timeout: 50 * time.Millisecond,
 					code:    http.StatusOK,
 					data:    "[0,1,2,3,4,5,6,7,8,9]\n",
