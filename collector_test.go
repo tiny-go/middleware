350b52b7312b7f02424eea31550230a819843e6e
Alexander Shcherbyna <xantax@ukr.net>
2018-02-25 14:29:35 +0100

    use existing funcs for testing

--- collector_test.go
+++ collector_test.go
@@ -5,24 +5,6 @@ import (
 	"testing"
 )
 
-var (
-	mwOne Middleware = func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
-		})
-	}
-	mwTwo Middleware = func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
-		})
-	}
-	mwThree Middleware = func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
-		})
-	}
-)
-
 func Test_List(t *testing.T) {
 	t.Run("Given a middleware List", func(t *testing.T) {
 		list := NewList()
@@ -30,7 +12,7 @@ func Test_List(t *testing.T) {
 			t.Error("middleware map should be empty after initialization")
 		}
 		t.Run("register middleware for HTTP method", func(t *testing.T) {
-			list.AddMiddleware(http.MethodGet, mwOne)
+			list.AddMiddleware(http.MethodGet, middlewareOne)
 			mw, ok := list.middleware[http.MethodGet]
 			if !ok {
 				t.Errorf("middleware map is expected to contain key %q", http.MethodGet)
@@ -40,7 +22,7 @@ func Test_List(t *testing.T) {
 			}
 		})
 		t.Run("add middleware func(s) to existing chain", func(t *testing.T) {
-			list.AddMiddleware(http.MethodGet, mwTwo, mwThree)
+			list.AddMiddleware(http.MethodGet, middlewareTwo, middlewareThree)
 			mw, ok := list.middleware[http.MethodGet]
 			if !ok {
 				t.Errorf("middleware map is expected to contain key %q", http.MethodGet)
@@ -58,13 +40,13 @@ func Test_List(t *testing.T) {
 		t.Run("get middleware registered for HTTP method", func(t *testing.T) {
 			mw := list.Middleware(http.MethodGet)
 			if mw[0] == nil {
-				t.Error("mwOne should not be nil")
+				t.Error("middlewareOne should not be nil")
 			}
 			if mw[1] == nil {
-				t.Error("mwTwo should not be nil")
+				t.Error("middlewareTwo should not be nil")
 			}
 			if mw[2] == nil {
-				t.Error("mwThree should not be nil")
+				t.Error("middlewareThree should not be nil")
 			}
 		})
 	})
