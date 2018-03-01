458a8668cac24689330ce2dab7fdb8edd325f726
Alexander Shcherbyna <xantax@ukr.net>
2018-03-01 09:23:17 +0100

    add Bearer tests

--- jwt_hs256_test.go
+++ jwt_hs256_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -13,6 +14,54 @@ import (
 type invalidClaims struct{}
 
 func (ic invalidClaims) Valid() error { return errors.New("error") }
+
+func Test_Bearer(t *testing.T) {
+	tokenString := "this-is-a-token"
+	t.Run("Given a function that should retrieve JWT from request", func(t *testing.T) {
+		t.Run("get a token from the request headers", func(t *testing.T) {
+			r, _ := http.NewRequest(http.MethodGet, "", nil)
+			r.Header.Set(jwtAuthKey, tokenString)
+			token, ok := Bearer(r)
+			if !ok {
+				t.Error("cannot retrieve token from request headers")
+			}
+			if token != tokenString {
+				t.Error("token contains invalid string")
+			}
+		})
+		t.Run("get a token from the request URI", func(t *testing.T) {
+			r, _ := http.NewRequest(http.MethodDelete, "?"+jwtAuthKey+"="+tokenString, nil)
+			token, ok := Bearer(r)
+			if !ok {
+				t.Error("cannot retrieve token from request URI")
+			}
+			if token != tokenString {
+				t.Error("token contains invalid string")
+			}
+		})
+		t.Run("getting a token from multipart form is not supported", func(t *testing.T) {
+			r, _ := http.NewRequest(http.MethodPost, "", nil)
+			r.Header.Add("Content-Type", "multipart/form-data")
+			_, ok := Bearer(r)
+			if ok {
+				t.Error("should not be able to retrieve the token")
+			}
+		})
+		t.Run("get a token from the request body sent as form-data", func(t *testing.T) {
+			form := url.Values{}
+			form.Add(jwtAuthKey, tokenString)
+			r, _ := http.NewRequest(http.MethodPost, "", strings.NewReader(form.Encode()))
+			r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			token, ok := Bearer(r)
+			if !ok {
+				t.Error("cannot retrieve token from request body")
+			}
+			if token != tokenString {
+				t.Errorf("token contains invalid string: %q", token)
+			}
+		})
+	})
+}
 
 func Test_JwtHS256(t *testing.T) {
 	type testCase struct {
@@ -63,10 +112,10 @@ func Test_JwtHS256(t *testing.T) {
 			secret:  "secret",
 			closure: func() jwt.Claims { return new(jwt.StandardClaims) },
 			headers: map[string]string{
-				jwtAuthKey: "eyJhbGciOiJ3cm9uZyIsInR5cCI6IkpXVCJ9.e30.ojMtefjRPf4M8NyHVOlR4fWq1TYkr8-2BUSKEddS45Y",
+				jwtAuthKey: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.e30.b-gLBOyB62jzeiETcpDg4wgLa9EcJcEN5Dh4Hna5Uvs6wqGWRco1uIxdsQJRTvsWPq63A_ZM9g7rjs-SEORyty1DqWNeqaK3uaECr5n80dL_oKcWUhzCDJbC2W_v4_2jQz4lz5m12FH-_N19RRymA_GeKuZMyvH0MUlitVfnjlA",
 			},
 			code: http.StatusForbidden,
-			body: "signing method (alg) is unavailable.\n",
+			body: "unexpected signing method: RS256\n",
 		},
 		{
 			title:   "token with invalid signature should return an error",
