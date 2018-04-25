7e73f0911d777bed8a3329f3f5e4ce2001e4f500
Alexander Shcherbyna <xantax@ukr.net>
2018-04-25 10:09:43 +0200

    move errors to separate package

--- panic_recover.go
+++ panic_recover.go
@@ -1,38 +1,51 @@
 package mw
 
 import (
-	"fmt"
 	"net/http"
 )
 
-// PanicHandler reports the error (panic) to the client as an HTTP error. If HTTP
-// error status code can be retrieved PanicHandler sends provided code to the client
-// instead of default (500). If provided panic (value) does not implement any of
-// supported interfaces (error/Error) - it tries to convert a panic to a string.
-func PanicHandler(w http.ResponseWriter, r interface{}) {
-	switch e := r.(type) {
-	case nil:
-		// panics that throw nil can be used to indicate that handler successfully
-		// completed the task and all of the next handlers will be ignored
-	case Error:
-		// retrieve status code and error message
-		http.Error(w, e.Error(), e.Code())
-	case error:
-		// all standard errors without codes will be sent as Internal Server Error
-		http.Error(w, e.Error(), http.StatusInternalServerError)
-	default:
-		// everything else
-		http.Error(w, fmt.Sprint(r), http.StatusInternalServerError)
-	}
-}
+// OnPanic is a function that should contain the logic responsible for any kind
+// of error/panic reporting (to the console, file, directly to the client or simply
+// ignore them).
+type OnPanic func(http.ResponseWriter, interface{})
 
 // PanicRecover returns a middleware that recovers from the panic.
-func PanicRecover(onFail func(http.ResponseWriter, interface{})) Middleware {
+//
+// A trivial example (retrieving an error from the panic and sending to the client
+// using errors package) is:
+//
+//  package main
+//
+//  import (
+//     	"log"
+// 	    "net/http"
+//
+// 	    "github.com/tiny-go/errors"
+// 	    "github.com/tiny-go/middleware"
+//  )
+//
+//  var (
+// 	    panicHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+// 		      panic("something went wrong")
+// 	    }
+//  )
+//
+//  func main() {
+// 	    http.Handle(
+// 		      "/",
+// 		      mw.
+// 			        // with HTTP panic handler
+// 			        New(mw.PanicRecover(errors.Send)).
+// 			        Then(panicHandler),
+// 	    )
+// 	    log.Fatal(http.ListenAndServe(":8080", nil))
+//  }
+func PanicRecover(onPanic OnPanic) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
 				// recover from panic and call the panic handler
-				defer func() { onFail(w, recover()) }()
+				defer func() { onPanic(w, recover()) }()
 				// call next middleware
 				next.ServeHTTP(w, r)
 			},
