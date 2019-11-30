01bf4a4e74413b553d066d60a2c73a34583ae0d1
Alex Shch <xantax@ukr.net>
2019-11-30 17:50:47 +0200

    move ErrHandler to tiny-go/errors

--- request_limit.go
+++ request_limit.go
@@ -1,14 +1,14 @@
 package mw
 
-import "net/http"
+import (
+	"net/http"
 
-// TODO: provide a choise either "wait" or "send error"
-
-// ErrHandler is a function that handles errors, for instance http.Error
-type ErrHandler func(w http.ResponseWriter, message string, code int)
+	"github.com/tiny-go/errors"
+)
 
 // RequestLimiter middleware apply concurrent request limit
-func RequestLimiter(fn ErrHandler, maxConcurrentRequests int) Middleware {
+// TODO: provide a choise either "wait" or "send error"
+func RequestLimiter(fn errors.HandlerFunc, maxConcurrentRequests int) Middleware {
 	limitChan := make(chan struct{}, maxConcurrentRequests)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +17,7 @@ func RequestLimiter(fn ErrHandler, maxConcurrentRequests int) Middleware {
 				defer func() { <-limitChan }()
 				next.ServeHTTP(w, r)
 			default:
-				fn(w, "too many requests", http.StatusTooManyRequests)
+				fn(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
 			}
 		})
 	}
